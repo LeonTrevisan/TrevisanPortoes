7c4dd4ea5dabcc5c38607017ec281a79ce489e4f
Leon <trevisanleon@gmail.com>
2026-01-27 16:51:45 -0300

    Alterar Status protótipo

---
+++ app/Models/AlterarStatus.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use App\Database\Connection;
+use PDO;
+
+class AlterarStatus
+{
+    private PDO $db;
+
+    public function desativarRegistro(string $tabela, int $id): void
+    {
+        
+    }
+}
