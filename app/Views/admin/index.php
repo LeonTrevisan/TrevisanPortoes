0d25910674d50c6c77df5865c170cc3a15ff6325
Leon <trevisanleon@gmail.com>
2026-01-30 12:17:16 -0300

    Remoção dos arquivos AlterarStatus

--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -5,19 +5,19 @@
     <td><?= $admin['email'] ?></td>
     <td>
         <div class="action-buttons">
-            <button onclick="editarAdmin(<?= $admin['id_admin'] ?>)">Editar</button>
+            <button class="btn btn-primary" onclick="editarAdmin(<?= $admin['id_admin'] ?>)">Editar</button>
 
             <?php if ($admin['deleted_at'] === null): ?>
-                <form method="POST" action="/desativar">
+                <form method="POST" action="/admin/desativar">
                     <input type="hidden" name="tipo" value="cliente">
                     <input type="hidden" name="id" value="<?= $cliente['id'] ?>">
-                    <button type="submit">Desativar</button>
+                    <button class="btn btn-danger" type="submit">Desativar</button>
                 </form>
             <?php else: ?>
-                <form method="POST" action="/ativar">
+                <form method="POST" action="/admin/ativar">
                     <input type="hidden" name="tipo" value="cliente">
                     <input type="hidden" name="id" value="<?= $cliente['id'] ?>">
-                    <button type="submit">Reativar</button>
+                    <button class="btn btn-sucess" type="submit">Reativar</button>
                 </form>
             <?php endif; ?>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Controllers\AdminController;
 $router->get('/admin', [AdminController::class, 'index']);
 $router->post('/admin/store', [AdminController::class, 'store']);
 
-$router->post('/ativar', [SoftDeleteController::class, 'ativar']);
-$router->post('/desativar', [SoftDeleteController::class, 'desativar']);
+$router->post('/softDelete/ativar', [SoftDeleteController::class, 'ativar']);
+$router->post('/softDelete/desativar', [SoftDeleteController::class, 'desativar']);
+
 
